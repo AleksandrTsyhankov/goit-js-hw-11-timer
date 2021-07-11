5a344821fd09ad400300cbd33589e560e77875f8
AleksandrTsyhankov <74210910+AleksandrTsyhankov@users.noreply.github.com>
2021-07-11 14:03:50 +0300

    -1st commit

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import timerTemplate from './templates/timer.hbs';
+const body = document.querySelector('body');
+body.insertAdjacentHTML('beforeend', timerTemplate());
+
+class CountdownTimer {
+    constructor({ selector, targetDate }) {
+        this.selector = selector;
+        this.targetDate = targetDate;
+
+        this.timerScreen = document.querySelector(this.selector);
+        this.daysCount = this.timerScreen.querySelector('span[data-value="days"]');
+        this.hoursCount = this.timerScreen.querySelector('span[data-value="hours"]');
+        this.minutesCount = this.timerScreen.querySelector('span[data-value="mins"]');
+        this.secondsCount = this.timerScreen.querySelector('span[data-value="secs"]');
+
+        this.start();
+    };
+
+    pad(value) {
+         return String(value).padStart(2, '0');
+};
+
+    calcTimer(time) {
+        const daysCalculated = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const hoursCalculated = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const minutesCalculated = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secondsCalculated = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    
+        return { daysCalculated, hoursCalculated, minutesCalculated, secondsCalculated };
+};
+
+    start() {     
+        const currentDate = new Date();
+        let time = currentDate - this.targetDate;
+
+        setInterval(() => {
+            time -= 1000;
+            const { daysCalculated, hoursCalculated, minutesCalculated, secondsCalculated } = this.calcTimer(time);
+            
+            this.daysCount.textContent = `${daysCalculated}`
+            this.hoursCount.textContent = `${hoursCalculated}`
+            this.minutesCount.textContent = `${minutesCalculated}`
+            this.secondsCount.textContent = `${secondsCalculated}`
+        }, 1000);
+    };
+};
+
+const timer = new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Jul 17, 2019'),
+});
+
